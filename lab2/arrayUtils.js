3d1c0a46ef79c3bdbe351563e77ccc7de5d5c87e
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-09-29 17:22:00 -0400

    Q1 got common elements without sorting

--- lab2/arrayUtils.js
+++ lab2/arrayUtils.js
@@ -4,9 +4,39 @@
 */
 
 let mergeCommonElements = (...args) => {
-  //this function takes in a variable number of arrays that's what the ...args signifies
+  if (args.length < 2) {
+    throw "You must Supply two arrays as input";
+  }
+
+  for (const arr of args) {
+    if (!Array.isArray(arr) || arr.length === 0) {
+      throw "Each input must be an array that is not empty.";
+    }
+  }
+
+  const flattenedArrays = args.map((arr) => arr.flat(Infinity));
+  
+  const commonElements = [...new Set(flattenedArrays[0])].filter((element) =>
+    flattenedArrays.every((arr) => arr.includes(element))
+  );
+
+  // Sort the common elements numerically first, then alphabetically for strings
+  // commonElements.sort((a, b) => {
+  //   if (typeof a === "number" && typeof b === "number") {
+  //     return a - b; // Sort numbers in ascending order
+  //   } else if (typeof a === "string" && typeof b === "string") {
+  //     return a.localeCompare(b); // Sort strings in ascending order
+  //   } else {
+  //     return 0; // Other types are not compared
+  //   }
+  // });
+   
+
+  return commonElements;
 };
 
 let findTriangles = (arr) => {};
 
 let stringMetrics = (arr) => {};
+
+export { mergeCommonElements, findTriangles, stringMetrics };
