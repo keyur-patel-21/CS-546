40c81c8ad417f831e49fbda590513c5fbc5ced92
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-09-30 17:59:27 -0400

    implemented basic Q7 with order error

--- lab2/objUtils.js
+++ lab2/objUtils.js
@@ -3,8 +3,84 @@
       DO NOT CHANGE THE FUNCTION NAMES
 */
 
-let solvePuzzles = (puzzles, pieces) => {};
+let solvePuzzles = (puzzles, pieces) => {
+  // Record the start time
+  const startTime = new Date();
+
+  // Check if puzzles exist and are of valid type (array)
+  if (!puzzles || !Array.isArray(puzzles)) {
+    throw new Error("Invalid input: Puzzles must be an array.");
+  }
+
+  // Check if puzzles is not an empty array
+  if (puzzles.length === 0) {
+    throw new Error("Invalid input: Puzzles array should not be empty.");
+  }
+
+  // Check if puzzles contains only objects, with at least one key/value in each object supplied
+  if (
+    !puzzles.every(
+      (obj) => typeof obj === "object" && Object.keys(obj).length > 0
+    )
+  ) {
+    throw new Error(
+      "Invalid input: Puzzles array should only contain non-empty objects."
+    );
+  }
+
+  // Check if each puzzle object in the puzzles array contains only the keys a-e
+  if (
+    !puzzles.every((obj) =>
+      Object.keys(obj).every((key) => /^[a-e]$/.test(key))
+    )
+  ) {
+    throw new Error(
+      "Invalid input: Each puzzle object should contain only keys a-e."
+    );
+  }
+
+  // Check if pieces is an object that has at least one key/value
+  if (
+    !pieces ||
+    typeof pieces !== "object" ||
+    Object.keys(pieces).length === 0
+  ) {
+    throw new Error(
+      "Invalid input: Pieces should be an object with at least one key/value."
+    );
+  }
+
+  // Check if each pieces object contains only the keys a-e
+  if (!Object.keys(pieces).every((key) => /^[a-e]$/.test(key))) {
+    throw new Error(
+      "Invalid input: Pieces object should contain only keys a-e."
+    );
+  }
+
+  // Solve the puzzles by adding missing pieces
+  const solvedPuzzles = puzzles.map((puzzle) => {
+    const completedPuzzle = { ...puzzle };
+
+    Object.keys(pieces).forEach((key) => {
+      if (!(key in completedPuzzle)) {
+        completedPuzzle[key] = pieces[key];
+      }
+    });
+
+    return completedPuzzle;
+  });
+
+  // Record the end time
+  const endTime = new Date();
+  const executionTime = endTime - startTime;
+
+  console.log(`Execution time: ${executionTime} milliseconds`);
+
+  return solvedPuzzles;
+};
 
 let evaluatePokerHand = (hand, communityCards) => {};
 
 let combineObjects = (arr) => {};
+
+export { solvePuzzles, evaluatePokerHand, combineObjects };
