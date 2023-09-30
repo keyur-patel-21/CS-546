b6421bfaf53ebc1fed7aa659a9846871b3c5c852
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-09-30 14:07:11 -0400

    implemented basic Q4 with expression

--- lab2/stringUtils.js
+++ lab2/stringUtils.js
@@ -3,8 +3,29 @@
       DO NOT CHANGE THE FUNCTION NAMES
 */
 
-let emojiCounter = (message) => {};
+let emojiCounter = (message) => {
+  // Check if message exists and is a string
+  if (!message || typeof message !== 'string') {
+    throw 'Invalid input: Please provide a non-empty string as the message.';
+  }
+
+  // Check if the message is not just an empty string with spaces
+  if (message.trim() === '') {
+    throw 'Invalid input: Message should not be an empty string with only spaces.';
+  }
+
+  // Regular expression to match valid emojis
+  const emojiRegex = /:[^:\s]+:/g;
+
+  // Extract emojis from the message
+  const emojis = message.match(emojiRegex);
+
+  // Return the count of valid emojis
+  return emojis ? emojis.length : 0;
+};
 
 let sortStockPrices = (lastStocks, currStocks) => {};
 
 let mashUp = (string1, string2) => {};
+
+export { emojiCounter, sortStockPrices, mashUp };
