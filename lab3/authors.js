ddd062c0e8d89b97c86e7b0986da7cd38e027586
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-10-02 17:38:42 -0400

    implemented Q1

--- lab3/authors.js
+++ lab3/authors.js
@@ -2,8 +2,32 @@
 //Authors data link: https://gist.githubusercontent.com/graffixnyc/a086a55e04f25e538b5d52a095fe4467/raw/e9f835e9a5439a647a24fa272fcb8f5a2b94dece/authors.json
 
 //you must use axios to get the data
+import axios from "axios";
 
-const getAuthorById = async (id) => {};
+async function getAuthors() {
+  const { data } = await axios.get(
+    "https://gist.githubusercontent.com/graffixnyc/a086a55e04f25e538b5d52a095fe4467/raw/e9f835e9a5439a647a24fa272fcb8f5a2b94dece/authors.json"
+  );
+  return data; // this will be the array of author objects
+}
+
+const authors = await getAuthors();
+
+const getAuthorById = async (id) => {
+  if (typeof id !== "string" || id.trim() === "") {
+    throw "The 'id' parameter is either absent or not a valid string."
+  }
+
+  id = id.trim();
+
+  const author = authors.find((a) => a.id === id);
+
+  if (!author) {
+    throw "Author not found";
+  }
+
+  return author;
+};
 
 const searchAuthorByName = async (searchTerm) => {};
 
@@ -12,3 +36,11 @@ const getBookNames = async (firstName, lastName) => {};
 const youngestOldest = async () => {};
 
 const sameBirthday = async (month, day) => {};
+
+export {
+  getAuthorById,
+  searchAuthorByName,
+  getBookNames,
+  youngestOldest,
+  sameBirthday,
+};
