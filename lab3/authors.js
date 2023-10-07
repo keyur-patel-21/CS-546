15a29dc429e030c5aa8a6d640bd1fd5a494eca67
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-10-07 18:24:47 -0400

    completed author.js except Q4

--- lab3/authors.js
+++ lab3/authors.js
@@ -9,6 +9,10 @@ const authors = await getAuthors();
 const books = await getBooks();
 
 const getAuthorById = async (id) => {
+  if (!id) {
+    throw "Provide id.";
+  }
+
   if (typeof id !== "string" || id.trim() === "") {
     throw "The 'id' parameter is either absent or not a valid string.";
   }
@@ -25,6 +29,10 @@ const getAuthorById = async (id) => {
 };
 
 const searchAuthorByName = async (searchTerm) => {
+  if (!searchTerm) {
+    throw "Provide searchTerm.";
+  }
+
   if (typeof searchTerm !== "string") {
     throw "searchTerm must be a string";
   }
@@ -55,6 +63,10 @@ const searchAuthorByName = async (searchTerm) => {
 };
 
 const getBookNames = async (firstName, lastName) => {
+  if (!firstName || !lastName) {
+    throw "Provide First and Last Name.";
+  }
+
   if (typeof firstName !== "string" || typeof lastName !== "string") {
     throw "firstName and lastName must be strings";
   }
@@ -88,9 +100,72 @@ const getBookNames = async (firstName, lastName) => {
   return bookTitles;
 };
 
-const youngestOldest = async () => {};
+const youngestOldest = async () => {
+  const authors = await getAuthors();
+
+  if (authors.length === 0) {
+    // Handle the case when there are no authors in the data.
+    return { youngest: null, oldest: null };
+  }
+
+  // Sort authors by date_of_birth in ascending order.
+  authors.sort((a, b) => {
+    const dateA = new Date(a.date_of_birth);
+    const dateB = new Date(b.date_of_birth);
+    return dateA - dateB;
+  });
+
+  const youngestAuthor = authors[0];
+  const oldestAuthor = authors[authors.length - 1];
+
+  // Check for ties in the youngest and oldest authors.
+  const youngestAuthors = [youngestAuthor];
+  const oldestAuthors = [oldestAuthor];
+
+  for (let i = 1; i < authors.length; i++) {
+    const author = authors[i];
+    const youngestDiff =
+      new Date(author.date_of_birth) - new Date(youngestAuthor.date_of_birth);
+    const oldestDiff =
+      new Date(author.date_of_birth) - new Date(oldestAuthor.date_of_birth);
+
+    if (youngestDiff === 0) {
+      youngestAuthors.push(author);
+    }
+
+    if (oldestDiff === 0) {
+      oldestAuthors.push(author);
+    }
+  }
+
+  // Sort tied authors by last name.
+  youngestAuthors.sort((a, b) => a.last_name.localeCompare(b.last_name));
+  oldestAuthors.sort((a, b) => a.last_name.localeCompare(b.last_name));
+
+  const youngestAuthorNames = youngestAuthors.map(
+    (author) => `${author.first_name} ${author.last_name}`
+  );
+  const oldestAuthorNames = oldestAuthors.map(
+    (author) => `${author.first_name} ${author.last_name}`
+  );
+
+  const result = {
+    youngest:
+      youngestAuthorNames.length === 1
+        ? youngestAuthorNames[0]
+        : youngestAuthorNames,
+    oldest:
+      oldestAuthorNames.length === 1 ? oldestAuthorNames[0] : oldestAuthorNames,
+  };
+
+  return result;
+};
 
 const sameBirthday = async (month, day) => {
+  if (!month || !month) {
+    throw "Provide month and day.";
+  }
+
   const monthNames = [
     "January",
     "February",
