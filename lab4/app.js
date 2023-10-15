e4a7175441a11d2492d1865922b3bc3de567cd05
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-10-14 22:10:39 -0400

    creted final app.js

--- lab4/app.js
+++ lab4/app.js
@@ -1,25 +1,262 @@
-import * as events from './data/events.js';
+import * as events from "./data/events.js";
+import { dbConnection, closeConnection } from "./config/mongoConnection.js";
+
+const db = await dbConnection();
+await db.dropDatabase();
+
+// // Q1
+// try {
+//   const patrickBBQ = await events.create(
+//     "Patrick's Big End of Summer BBQ",
+//     "Come join us for our yearly end of summer bbq!",
+//     {
+//       streetAddress: "1 Castle Point Terrace",
+//       city: "Hoboken",
+//       state: "NJ",
+//       zip: "07030",
+//     },
+//     "phill@stevens.edu",
+//     30,
+//     0,
+//     "08/25/2024",
+//     "2:00PM",
+//     "8:00PM",
+//     false
+//   );
+//   console.log(patrickBBQ);
+// } catch (e) {
+//   console.log(e);
+// }
+
+// try {
+//   const patrickBBQ = await events.create(
+//     "Keyur's Big End of Summer BBQ",
+//     "Come join us for our yearly end of summer bbq!",
+//     {
+//       streetAddress: "2 Castle Point Terrace",
+//       city: "Islein",
+//       state: "NJ",
+//       zip: "07030",
+//     },
+//     "kpatel@stevens.edu",
+//     30,
+//     0,
+//     "08/19/2024",
+//     "2:00PM",
+//     "8:00PM",
+//     false
+//   );
+//   console.log(patrickBBQ);
+// } catch (e) {
+//   console.log(e);
+// }
+
+// Q2
+// try {
+//   const allEvents = await events.getAll();
+//   console.log(allEvents);
+// } catch (e) {
+//   console.log(e);
+// }
+
+// Q3
+// try{
+//   const patricksBBQ = await events.get("652b2495e47462d0aa84725b");
+// console.log(patricksBBQ);
+// }catch(e){
+//   console.log(e)
+// }
+
+// Q4
+// try {
+//   const removeAidensBDayBash = await events.remove("652b2495e47462d0aa84725a");
+//   console.log(removeAidensBDayBash);
+// } catch (e) {
+//   console.log(e);
+// }
+
+// Q5
+// try {
+//   const renamedAidensbdayBash = await events.rename(
+//     "652b2495e47462d0aa84725b",
+//     "Aiden's 5th Birthday Bash"
+//   );
+//   console.log(renamedAidensbdayBash);
+// } catch (e) {
+//   console.log(e);
+// }
 
 // 1. Create a event of your choice.
+// 2. Log the newly created event. (Just that event, not all events)
+try {
+  const patrickBBQ = await events.create(
+    "Keyur's Big End of Summer BBQ",
+    "Come join us for our yearly end of summer bbq!",
+    {
+      streetAddress: "2 Castle Point Terrace",
+      city: "Islein",
+      state: "NJ",
+      zip: "07030",
+    },
+    "kpatel@stevens.edu",
+    30,
+    0,
+    "08/19/2024",
+    "2:00PM",
+    "8:00PM",
+    false
+  );
+  console.log(patrickBBQ);
+} catch (e) {
+  console.log(e);
+}
+// 3. Create another event of your choice.
+try {
+  const patrickBBQ = await events.create(
+    "John's Big End of Summer BBQ",
+    "Come join us for our yearly end of summer bbq!",
+    {
+      streetAddress: "2 Castle Point Terrace",
+      city: "Long Beach",
+      state: "LA",
+      zip: "07030",
+    },
+    "kpatel@stevens.edu",
+    30,
+    0,
+    "08/19/2024",
+    "2:00PM",
+    "8:00PM",
+    false
+  );
+} catch (e) {
+  console.log(e);
+}
+// 4. Query all events, and log them all
+try {
+  const allEvents = await events.getAll();
+  console.log(allEvents);
+} catch (e) {
+  console.log(e);
+}
+// 5. Create the 3rd event of your choice.
+// 6. Log the newly created 3rd event. (Just that event, not all events)
+try {
+  const patrickBBQ = await events.create(
+    "Thi is third added Event",
+    "go join us for our yearly end of summer Sale!",
+    {
+      streetAddress: "3 Castle Point Terrace",
+      city: "gopal",
+      state: "GJ",
+      zip: "07039",
+    },
+    "ddesai@stevens.edu",
+    30,
+    0,
+    "08/19/2024",
+    "2:00PM",
+    "8:00PM",
+    false
+  );
+  console.log(patrickBBQ);
+} catch (e) {
+  console.log(e);
+}
+// 7. Rename the first event
+// 8. Log the first event with the updated name.
+try {
+  const renamedAidensbdayBash = await events.rename(
+    "652b2495e47462d0aa84725b",
+    "Aiden's 5th Birthday Bash"
+  );
+  console.log(renamedAidensbdayBash);
+} catch (e) {
+  console.log(e);
+}
+// 9. Remove the second event you created.
+try {
+  const removeAidensBDayBash = await events.remove("652b2495e47462d0aa84725a");
+} catch (e) {
+  console.log(e);
+}
+// 10. Query all events, and log them all
+try {
+  const allEvents = await events.getAll();
+  console.log(allEvents);
+} catch (e) {
+  console.log(e);
+}
 
-const patrickBBQ = await events.create("Patrick's Big End of Summer BBQ","Come join us for our yearly end of summer bbq!",{streetAddress: "1 Castle Point Terrace", city: "Hoboken", state: "NJ", zip: "07030"}, "phill@stevens.edu",30,0,"08/25/2024","2:00PM","8:00PM",false);
-console.log(patrickBBQ);
+// 11. Try to create an event with bad input parameters to make sure it throws errors.
+try {
+  const patrickBBQ = await events.create(
+    "Keyur's Big End of Summer BBQ",
+    "Come join us ",
+    {
+      streetAddress: "2 Castle Point Terrace",
+      city: "Islein",
+      state: "NJ",
+      zip: "07030",
+    },
+    "kpatel@stevens.edu",
+    30,
+    0,
+    "08/19/2024",
+    "2:00PM",
+    "8:00PM",
+    false
+  );
+} catch (e) {
+  console.log(e);
+}
 
-/*
-    1. Create a event of your choice.
+// 12. Try to remove an event that does not exist to make sure it throws errors.
+try {
+  const removeAidensBDayBash = await events.remove(
+    "652b2495e47462d0aa84725aa2b"
+  );
+} catch (e) {
+  console.log(e);
+}
+// 13. Try to rename an event that does not exist to make sure it throws errors.
+try {
+  const renamedAidensbdayBash = await events.rename(
+    "652b2495e47462d0adf4a84725b",
+    "Aiden's 5th Birthday Bash"
+  );
+} catch (e) {
+  console.log(e);
+}
+// 14. Try to rename an event passing in invalid data for the newEventName parameter to make sure it throws errors.
+try {
+  const patrickBBQ = await events.create(
+    "Key",
+    "Come join us for our yearly end of summer bbq!",
+    {
+      streetAddress: "2 Castle Point Terrace",
+      city: "Islein",
+      state: "NJ",
+      zip: "07030",
+    },
+    "kpatel@stevens.edu",
+    30,
+    0,
+    "08/19/2024",
+    "2:00PM",
+    "8:00PM",
+    false
+  );
+  console.log(patrickBBQ);
+} catch (e) {
+  console.log(e);
+}
 
-    2. Log the newly created event. (Just that event, not all events)
-    3. Create another event of your choice.
-    4. Query all events, and log them all
-    5. Create the 3rd event of your choice.
-    6. Log the newly created 3rd event. (Just that event, not all events)
-    7. Rename the first event
-    8. Log the first event with the updated name. 
-    9. Remove the second event you created.
-    10. Query all events, and log them all
-    11. Try to create an event with bad input parameters to make sure it throws errors.
-    12. Try to remove an event that does not exist to make sure it throws errors.
-    13. Try to rename an event that does not exist to make sure it throws errors.
-    14. Try to rename an event passing in invalid data for the newEventName parameter to make sure it throws errors.
-    15. Try getting an event by ID that does not exist to make sure it throws errors.
-*/
+// 15. Try getting an event by ID that does not exist to make sure it throws errors.
+try {
+  const patricksBBQ = await events.get("652b2495e47462d0aadgde84725b");
+} catch (e) {
+  console.log(e);
+}
+
+await closeConnection();
