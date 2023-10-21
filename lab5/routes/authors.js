dc19dc88afe4c84373281c3c950136fc5dcc6fa1
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-10-21 15:09:41 -0400

    implemented authors.js

--- lab5/routes/authors.js
+++ lab5/routes/authors.js
@@ -3,12 +3,28 @@
 
 //You can import your getAuthors() function in the /data/data.js file that you used for lab 3 to return the list of authors and call it in the /authors route.  You can also import your getAuthorById(id) function and call it in the :/id route.
 
+import express from "express";  
 const router = express.Router();
+import { authors, getAuthorById } from "../data/data.js";
 
-router.route('/');
-// Implement GET Request Method and send a JSON response See lecture code!
+// Implement GET Request Method and send a JSON response
+router.route("/").get(async (req, res) => {
+  try {
+    const authorList = await authors;
+    return res.json(authorList);
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
 
-router.route('/:id');
-//Implement GET Request Method and send a JSON response See lecture code!
+// Implement GET Request Method and send a JSON response
+router.route("/:id").get(async (req, res) => {
+  try {
+    const post = await getAuthorById(req.params.id);
+    return res.json(post);
+  } catch (e) {
+    return res.status(404).json(e);
+  }
+});
 
 export default router;
