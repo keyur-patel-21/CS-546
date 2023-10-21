f7b40a625107eb01179919705107e2c112ae8c31
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-10-21 15:09:52 -0400

    implemented books.js

--- lab5/routes/books.js
+++ lab5/routes/books.js
@@ -2,12 +2,29 @@
 //Lecture Code Refernece -> https://github.com/stevens-cs546-cs554/CS-546/tree/master/lecture_05/routes
 
 //You can import your getBooks() function in the /data/data.js file that you used for lab 3 to return the list of books.  You can also import your getBookById(id) function and call it in the :/id route.
+
+import express from "express";  
 const router = express.Router();
+import { books, getBookById } from "../data/data.js";
 
-router.route('/');
 // Implement GET Request Method and send a JSON response  See lecture code!
+router.route("/").get(async (req, res) => {
+  try {
+    const bookList = await books;
+    return res.json(bookList);
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
 
-router.route('/:id');
 // Implement GET Request Method and send a JSON response See lecture code!
+router.route("/:id").get(async (req, res) => {
+  try {
+    const post = await getBookById(req.params.id);
+    return res.json(post);
+  } catch (e) {
+    return res.status(404).json(e);
+  }
+});
 
 export default router;
