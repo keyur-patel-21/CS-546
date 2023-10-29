3833f1aa08666aea8d3120a27c8ab52ca6b968ee
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-10-29 11:37:57 -0400

    changed DB name

--- lab6/config/settings.js
+++ lab6/config/settings.js
@@ -1,5 +1,5 @@
 // You will need to change the DB name to match the required DB name in the assignment specs!
 export const mongoConfig = {
-  serverUrl: 'mongodb://localhost:27017/',
-  database: 'DB_NAME_GOES_HERE'
+  serverUrl: 'mongodb://127.0.0.1:27017/',
+  database: 'Keyur_Patel_lab6'
 };
