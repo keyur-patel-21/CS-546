80c4a3ebd4f884b1cc179918ebd66b95de97c86a
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-10-29 11:49:01 -0400

    copied event create functoin from lab4

--- lab6/data/events.js
+++ lab6/data/events.js
@@ -12,8 +12,107 @@ const create = async (
   endTime,
   publicEvent
 ) => {
-  //Implement Code here
-  //Do NOT forget to initalize attendees to be an empty array and totalNumberOfAttendees to 0 on event creation
+  if (typeof eventName !== "string" || eventName.trim().length < 5) {
+    throw "Invalid eventName";
+  }
+
+  if (
+    typeof eventDescription !== "string" ||
+    eventDescription.trim().length < 25
+  ) {
+    throw "Invalid eventDescription";
+  }
+
+  if (!isValidEmail(contactEmail)) {
+    throw "Invalid contactEmail";
+  }
+
+  if (!isValidDate(eventDate)) {
+    throw "Invalid eventDate";
+  }
+
+  if (!isValidTime(startTime)) {
+    throw "Invalid startTime";
+  }
+
+  if (!isValidTime(endTime)) {
+    throw "Invalid endTime";
+  }
+
+  const eventDateObj = new Date(eventDate);
+  const startTimeObj = new Date(`01/01/2000 ${startTime}`);
+  const endTimeObj = new Date(`01/01/2000 ${endTime}`);
+
+  if (eventDateObj <= new Date()) {
+    throw "EventDate must be in the future";
+  }
+
+  if (startTimeObj >= endTimeObj) {
+    throw "Invalid time range";
+  }
+
+  const timeDifference = endTimeObj - startTimeObj;
+  if (timeDifference < 30 * 60 * 1000) {
+    throw "endTime should be at least 30 minutes later than startTime";
+  }
+
+  if (typeof publicEvent !== "boolean") {
+    throw "Invalid publicEvent";
+  }
+
+  if (
+    typeof maxCapacity !== "number" ||
+    typeof priceOfAdmission !== "number" ||
+    maxCapacity <= 0 ||
+    priceOfAdmission < 0
+  ) {
+    throw "Invalid maxCapacity or priceOfAdmission";
+  }
+
+  if (typeof eventLocation !== "object") {
+    throw "Invalid eventLocation";
+  }
+
+  if (
+    typeof eventLocation.streetAddress !== "string" ||
+    eventLocation.streetAddress.trim().length < 3 ||
+    typeof eventLocation.city !== "string" ||
+    eventLocation.city.trim().length < 3 ||
+    typeof eventLocation.state !== "string" ||
+    !isValidState(eventLocation.state) ||
+    typeof eventLocation.zip !== "string" ||
+    !isValidZip(eventLocation.zip)
+  ) {
+    throw "Invalid eventLocation properties";
+  }
+
+  const newEvent = {
+    eventName: eventName,
+    description: eventDescription,
+    eventLocation: {
+      streetAddress: eventLocation.streetAddress,
+      city: eventLocation.city,
+      state: eventLocation.state,
+      zip: eventLocation.zip,
+    },
+    contactEmail: contactEmail,
+    maxCapacity: maxCapacity,
+    priceOfAdmission: priceOfAdmission,
+    eventDate: eventDate,
+    startTime: startTime,
+    endTime: endTime,
+    publicEvent: publicEvent,
+  };
+
+  const eventCollection = await events();
+
+  const insertInfo = await eventCollection.insertOne(newEvent);
+  if (!insertInfo.acknowledged || !insertInfo.insertedId)
+    throw "Could not add Event";
+  const newId = insertInfo.insertedId.toString();
+
+  const event = await get(newId);
+  return event;
 };
 
 const getAll = async () => {
