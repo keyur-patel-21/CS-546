b3486a0aa11ff2e6e52d4be137bd4ba441b42db4
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-10-29 18:08:08 -0400

    implemented basic getAll event route

--- lab6/routes/events.js
+++ lab6/routes/events.js
@@ -1,10 +1,19 @@
 // Import the express router as shown in the lecture code
 // Note: please do not forget to export the router!
+import {Router} from 'express';
+const router = Router();
+import {eventData} from '../data/index.js';
+// import validation from '../validation.js';
 
 router
-  .route('/')
+  .route('/events')
   .get(async (req, res) => {
-    //code here for GET
+    try {
+      const eventList = await eventData.getAll();
+      res.json(eventList);
+    } catch (e) {
+      res.status(500).json({error: e});
+    }
   })
   .post(async (req, res) => {
     //code here for POST
