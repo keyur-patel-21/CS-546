2d23fdee7dbd0408b658c924e6027773f386965c
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-10-31 20:51:08 -0400

    exported router

--- lab6/routes/attendees.js
+++ lab6/routes/attendees.js
@@ -1,8 +1,19 @@
 // Import the express router as shown in the lecture code
 // Note: please do not forget to export the router!
-
+import { Router } from "express";
+const router = Router();
+import { eventData } from "../data/index.js";
+import { attendeeData } from "../data/index.js";
+import {
+  isValidEmail,
+  isValidDate,
+  isValidTime,
+  isValidState,
+  isValidZip,
+  checkId,
+} from "../helpers.js";
 router
-  .route('/:eventId')
+  .route("/:eventId")
   .get(async (req, res) => {
     //code here for GET
   })
@@ -11,10 +22,12 @@ router
   });
 
 router
-  .route('/attendee/:attendeeId')
+  .route("/attendee/:attendeeId")
   .get(async (req, res) => {
     //code here for GET
   })
   .delete(async (req, res) => {
     //code here for DELETE
   });
+
+export default router;
