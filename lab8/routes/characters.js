b763e5bd98eaf9eead459ef852637cddb5b51fe7
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-11-12 10:40:35 -0500

    indentation fix

--- lab8/routes/characters.js
+++ lab8/routes/characters.js
@@ -15,13 +15,21 @@ router.route("/").get(async (req, res) => {
 router.route("/searchmarvelcharacters").post(async (req, res) => {
   //code here for POST this is where your form will be submitting searchCharacterByName and then call your data function passing in the searchCharacterByName and then rendering the search results of up to 15 characters.
   try {
-    req.params.name = helper.checkString(req.params.name, "name");
+    req.params.searchCharacterByName = helper.checkString(
+      req.params.searchCharacterByName,
+      "searchCharacterByName"
+    );
   } catch (e) {
     return res.status(400).json({ error: e.message });
   }
   try {
-    const characterList = await searchCharacterByName(req.params.name);
-    res.render("characterSearchResults", { characters: characterList });
+    const characterList = await searchCharacterByName(
+      req.params.searchCharacterByName
+    );
+    res.render("characterSearchResults", {
+      characters: characterList,
+      name: req.params.searchCharacterByName,
+    });
   } catch (e) {
     res.status(400).json({ error: e.message });
   }
