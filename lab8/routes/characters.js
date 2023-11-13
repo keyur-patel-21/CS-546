7972d94f35c69021f82c825084c336ac5f4b5375
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-11-12 19:59:57 -0500

    fix: route as lab8

--- lab8/routes/characters.js
+++ lab8/routes/characters.js
@@ -12,55 +12,68 @@ router.route("/").get(async (req, res) => {
   res.render("home");
 });
 
-// router.route("/searchmarvelcharacters").post(async (req, res) => {
-//   //code here for POST this is where your form will be submitting searchCharacterByName and then call your data function passing in the searchCharacterByName and then rendering the search results of up to 15 characters.
-//   try {
-//     req.params.searchCharacterByName = helper.checkString(
-//       req.params.searchCharacterByName,
-//       "searchCharacterByName"
-//     );
-//   } catch (e) {
-//     return res.status(400).json({ error: e.message });
-//   }
-//   try {
-//     const characterList = await searchCharacterByName(
-//       req.params.searchCharacterByName
-//     );
-//     res.render("charactersearchresults", {
-//       characters: characterList,
-//       name: req.params.searchCharacterByName,
-//     });
-//   } catch (e) {
-//     res.status(400).json({ error: e.message });
-//   }
-// });
-
 router.route("/searchmarvelcharacters").post(async (req, res) => {
+  //code here for POST this is where your form will be submitting searchCharacterByName and then call your data function passing in the searchCharacterByName and then rendering the search results of up to 15 characters.
   let searchData = req.body;
-  let errors = [];
-  var regEx = /^[a-zA-Z]+$/;
-
-  if(!searchData.searchCharacterByName || typeof searchData.searchCharacterByName !== 'string' || !searchData.searchCharacterByName.trim().replace(/\s/g, "").length) {
-    errors.push('Please enter valid string for your search');
-  }
-  else if(!(regEx.test(searchData.searchCharacterByName))){
-    errors.push('Search Data should contains alphabets only');
-  }
-
-  if(errors.length){
-    res.statusCode=400;
-    res.render('error', { errors:errors, title: "error"});
-    return;
+  try {
+    searchData.searchCharacterByName = helper.checkString(
+      searchData.searchCharacterByName,
+      "searchCharacterByName"
+    );
+  } catch (e) {
+    return res.status(400).json({ error: e.message });
   }
   try {
-    const characterList =  await searchCharacterByName(searchData.searchCharacterByName);  
-    res.render('characterSearchResults', {searchCharacterByName: searchData.searchCharacterByName, characters: characterList.data.results, title: "Character Found"});
+    const characterList = await searchCharacterByName(
+      searchData.searchCharacterByName
+    );
+    res.render("characterSearchResults", {
+      searchCharacterByName: searchData.searchCharacterByName,
+      characters: characterList.data.results,
+      title: "Character Found",
+    });
   } catch (e) {
-    res.statusCode=404;
-    res.render('error', {searchCharacterByName: searchData.searchCharacterByName, title: "No Character Found" })
+    res.status(400).json({ error: e.message });
   }
 });
 
+// router.route("/searchmarvelcharacters").post(async (req, res) => {
+//   let searchData = req.body;
+//   let errors = [];
+//   var regEx = /^[a-zA-Z]+$/;
+
+//   if (
+//     !searchData.searchCharacterByName ||
+//     typeof searchData.searchCharacterByName !== "string" ||
+//     !searchData.searchCharacterByName.trim().replace(/\s/g, "").length
+//   ) {
+//     errors.push("Please enter valid string for your search");
+//   } else if (!regEx.test(searchData.searchCharacterByName)) {
+//     errors.push("Search Data should contains alphabets only");
+//   }
+
+//   if (errors.length) {
+//     res.statusCode = 400;
+//     res.render("error", { errors: errors, title: "error" });
+//     return;
+//   }
+//   try {
+//     const characterList = await searchCharacterByName(
+//       searchData.searchCharacterByName
+//     );
+//     res.render("characterSearchResults", {
+//       searchCharacterByName: searchData.searchCharacterByName,
+//       characters: characterList.data.results,
+//       title: "Character Found",
+//     });
+//   } catch (e) {
+//     res.statusCode = 404;
+//     res.render("error", {
+//       searchCharacterByName: searchData.searchCharacterByName,
+//       title: "No Character Found",
+//     });
+//   }
+// });
 
 router.route("/marvelcharacter/:id").get(async (req, res) => {
   //code here for GET a single character
