a088e4b0b52d9b3039539f0be3dddf05dae0e58f
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-11-19 13:45:16 -0500

    implemented basic routes

--- lab9/routes/index.js
+++ lab9/routes/index.js
@@ -1,1 +1,12 @@
 //Here you will require route files and export them as used in previous labs.
+import pelindromeCheckRoutes from './palindromeCheck.js';
+
+const constructorMethod = (app) => {
+  app.use('/', pelindromeCheckRoutes);
+
+  app.use('*', (req, res) => {
+    res.redirect('/static/homepage');
+  });
+};
+
+export default constructorMethod;
--- lab9/routes/palindromeCheck.js
+++ lab9/routes/palindromeCheck.js
@@ -4,3 +4,12 @@ Your server this week should not be doing any of the processing! Your server onl
 
 you just need one route to send the static homepage.html file
 */
+
+import {Router} from 'express';
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.render('static/homepage', {partial: 'static_script'});
+});
+
+export default router;
