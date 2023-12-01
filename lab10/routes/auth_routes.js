c2a970f9ad0463703dc2ed4d5561bd93ff1bc32f
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-12-01 13:21:16 -0500

    implemented basic login and register routes

--- lab10/routes/auth_routes.js
+++ lab10/routes/auth_routes.js
@@ -1,40 +1,195 @@
 //import express, express router as shown in lecture code
+import { Router } from "express";
+const router = Router();
+import { registerUser, loginUser } from "../data/index.js";
 
-router.route('/').get(async (req, res) => {
+// Function to validate email address format
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+// Function to validate password format
+function isValidPassword(password) {
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return passwordRegex.test(password);
+}
+
+router.route("/").get(async (req, res) => {
   //code here for GET THIS ROUTE SHOULD NEVER FIRE BECAUSE OF MIDDLEWARE #1 IN SPECS.
-  return res.json({error: 'YOU SHOULD NOT BE HERE!'});
+  return res.json({ error: "YOU SHOULD NOT BE HERE!" });
 });
 
 router
-  .route('/register')
+  .route("/register")
   .get(async (req, res) => {
     //code here for GET
+    res.render("register");
   })
   .post(async (req, res) => {
-    //code here for POST
+    const {
+      firstNameInput,
+      lastNameInput,
+      emailAddressInput,
+      passwordInput,
+      confirmPasswordInput,
+      roleInput,
+    } = req.body;
+
+    // Check if required fields are present
+    if (
+      !firstNameInput ||
+      !lastNameInput ||
+      !emailAddressInput ||
+      !passwordInput ||
+      !confirmPasswordInput ||
+      !roleInput
+    ) {
+      return res
+        .status(400)
+        .render("register", { error: "All fields are required" });
+    }
+
+    // Validate firstNameInput
+    if (!/^[a-zA-Z]{2,25}$/.test(firstNameInput)) {
+      return res
+        .status(400)
+        .render("register", { error: "Invalid firstNameInput" });
+    }
+
+    // Validate lastNameInput
+    if (!/^[a-zA-Z]{2,25}$/.test(lastNameInput)) {
+      return res
+        .status(400)
+        .render("register", { error: "Invalid lastNameInput" });
+    }
+
+    // Validate emailAddressInput
+    if (!isValidEmail(emailAddressInput)) {
+      return res
+        .status(400)
+        .render("register", { error: "Invalid emailAddressInput" });
+    }
+
+    // Validate passwordInput
+    if (!isValidPassword(passwordInput)) {
+      return res
+        .status(400)
+        .render("register", { error: "Invalid passwordInput" });
+    }
+
+    // Confirm password match
+    if (passwordInput !== confirmPasswordInput) {
+      return res
+        .status(400)
+        .render("register", {
+          error: "Password and confirmPassword do not match",
+        });
+    }
+
+    // Validate roleInput
+    if (roleInput !== "admin" && roleInput !== "user") {
+      return res.status(400).render("register", { error: "Invalid roleInput" });
+    }
+
+    try {
+      // Call your registerUser db function passing in the fields from the request.body
+      const result = await registerUser({
+        firstName: firstNameInput,
+        lastName: lastNameInput,
+        email: emailAddressInput,
+        password: passwordInput,
+        role: roleInput,
+      });
+
+      // Check if the DB function returns {insertedUser: true}
+      if (result.insertedUser) {
+        // Redirect to the /login page
+        res.redirect("/login");
+      } else {
+        // Respond with a status code of 500 and an error message
+        res.status(500).send("Internal Server Error");
+      }
+    } catch (error) {
+      // Render the sign-up screen with an error message and an HTTP 400 status code
+      res.status(400).render("register", { error: error.message });
+    }
   });
 
 router
-  .route('/login')
+  .route("/login")
   .get(async (req, res) => {
-    //code here for GET
+    // Code here for GET
+    res.render("login");
   })
   .post(async (req, res) => {
-    //code here for POST
+    const { emailAddressInput, passwordInput } = req.body;
+
+    // Check if required fields are present
+    if (!emailAddressInput || !passwordInput) {
+      return res
+        .status(400)
+        .render("login", { error: "Email address and password are required" });
+    }
+
+    // Validate emailAddressInput
+    if (!isValidEmail(emailAddressInput)) {
+      return res
+        .status(400)
+        .render("login", { error: "Invalid email address format" });
+    }
+
+    try {
+      // Call your loginUser db function passing in the emailAddressInput and passwordInput from the request.body
+      const user = await loginUser(
+        emailAddressInput.toLowerCase(),
+        passwordInput
+      );
+
+      // Check if the DB function returns the user
+      if (user) {
+        // Set cookie named AuthState
+        res.cookie("AuthState", true);
+
+        // Store user information in req.session.user
+        req.session.user = {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          emailAddress: user.email,
+          role: user.role,
+        };
+
+        // Redirect based on user role
+        if (user.role === "admin") {
+          res.redirect("/admin");
+        } else {
+          res.redirect("/protected");
+        }
+      } else {
+        // Render the login screen with an error message and an HTTP 400 status code
+        res
+          .status(400)
+          .render("login", { error: "Invalid email address and/or password" });
+      }
+    } catch (error) {
+      // Render the login screen with an error message and an HTTP 400 status code
+      res.status(400).render("login", { error: error.message });
+    }
   });
 
-router.route('/protected').get(async (req, res) => {
+router.route("/protected").get(async (req, res) => {
   //code here for GET
 });
 
-router.route('/admin').get(async (req, res) => {
+router.route("/admin").get(async (req, res) => {
   //code here for GET
 });
 
-router.route('/error').get(async (req, res) => {
+router.route("/error").get(async (req, res) => {
   //code here for GET
 });
 
-router.route('/logout').get(async (req, res) => {
+router.route("/logout").get(async (req, res) => {
   //code here for GET
 });
