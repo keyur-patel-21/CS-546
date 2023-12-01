8e9c292e7c2f57051875171e77cbe4b0689d1a69
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-12-01 13:43:54 -0500

    implemented index.js in routes

--- lab10/routes/index.js
+++ lab10/routes/index.js
@@ -1,1 +1,13 @@
 //Here you will import route files and export the constructor method as shown in lecture code and worked in previous labs.
+
+import authRoutes from "./auth_routes.js";
+
+const constructorMethod = (app) => {
+  app.use("/", authRoutes);
+
+  app.use("*", (req, res) => {
+    res.status(404).json({ error: "Route Not found" });
+  });
+};
+
+export default constructorMethod;
