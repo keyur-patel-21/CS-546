44c093e650b551e08ab86c986b18ec35d958640a
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-12-01 21:39:18 -0500

    fix : input name

--- lab10/public/js/client_side_validation.js
+++ lab10/public/js/client_side_validation.js
@@ -1,6 +1,3 @@
-// In this file, you must perform all client-side validation for every single form input (and the role dropdown) on your pages. The constraints for those fields are the same as they are for the data functions and routes. Using client-side JS, you will intercept the form's submit event when the form is submitted and If there is an error in the user's input or they are missing fields, you will not allow the form to submit to the server and will display an error on the page to the user informing them of what was incorrect or missing.  You must do this for ALL fields for the register form as well as the login form. If the form being submitted has all valid data, then you will allow it to submit to the server for processing. Don't forget to check that password and confirm password match on the registration form!
-
-
 // client_side_validation.js
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -66,8 +63,8 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function validateLoginForm() {
-    const email = document.getElementById("emailAddressInputLogin").value;
-    const password = document.getElementById("passwordInputLogin").value;
+    const email = document.getElementById("emailAddressInput").value;
+    const password = document.getElementById("passwordInput").value;
 
     if (!email || !password) {
       displayError("Email address and password are required");
