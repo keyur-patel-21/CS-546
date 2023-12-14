7b9decbc392b39d028d0daed25f258a14b3a1e92
keyur-at-simform <keyur.patel@simformsolutions.com>
2023-12-14 14:29:14 -0500

    fix: back to all show not visible on search result

--- lab11/public/js/main.js
+++ lab11/public/js/main.js
@@ -36,6 +36,7 @@ $(document).ready(function () {
           success: function (data) {
               // Populate tvShowList with search results
               populateShowList(data);
+              $("#rootLink").show();
           },
           error: function (error) {
               console.log("Error fetching search results: ", error);
@@ -76,33 +77,28 @@ $(document).ready(function () {
 });
 
 function populateShowList(data) {
-    let tvShowList = $("#tvShowList");
-  
-    // Check if the data has a 'self' property, indicating it's from the initial API
-    if (data[0] && data[0]._links && data[0]._links.self) {
-      // Iterate through the shows and create list items with links
-      data.forEach(function (show) {
-        let listItem = $("<li></li>");
-        let link = $("<a></a>").attr("href", show._links.self.href).text(show.name);
-  
-        listItem.append(link);
-        tvShowList.append(listItem);
-      });
-    } else {
-      // Handle the data structure from the search API
-      data.forEach(function (result) {
-        let show = result.show;
-        let listItem = $("<li></li>");
-        let link = $("<a></a>").attr("href", show._links.self.href).text(show.name);
-  
-        listItem.append(link);
-        tvShowList.append(listItem);
-      });
-    }
-  
-    // Show the tvShowList
-    tvShowList.show();
-  }
+  let tvShowList = $("#tvShowList");
+
+  // Check if the data has a 'self' property, indicating it's from the initial API
+  let isInitialList = data[0] && data[0]._links && data[0]._links.self;
+
+  // Iterate through the shows and create list items with links
+  data.forEach(function (item) {
+    let show = isInitialList ? item : item.show;
+    let listItem = $("<li></li>");
+    let link = $("<a></a>").attr("href", show._links.self.href).text(show.name);
+
+    listItem.append(link);
+    tvShowList.append(listItem);
+  });
+
+  // Show the tvShowList
+  tvShowList.show();
+
+  // Show or hide the "Back to All Shows" link
+  $("#rootLink").toggle(isInitialList);
+}
+
   
   
 
